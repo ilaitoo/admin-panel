e6bc6a44992e9fb27a8a5f5f0ec528eeedc304b2
mohammed <vip.x.3047@gmail.com>
2025-03-15 07:01:38 +0300

    Fixed dependencies

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,12 @@
 import LoginForm from "@/components/login/LoginForm";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <>
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </>
   );
 }
